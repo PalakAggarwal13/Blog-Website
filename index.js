b91da6d7fd99be5a89ca9b3581c6f7c8813503f3
Palak aggarwal <palakaggarwalaol@gmail.com>
2025-05-30 18:45:10 +0530

    check for authentication and generate cookie

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require ("path");
 const userRoute = require("./routes/user");
+const cookieParser = require("cookie-parser");
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
+
 
 const app = express();
 const PORT = 8000;
@@ -13,9 +16,13 @@ app.set("view engine","ejs");
 app.set("views", path.resolve("./views"));
 
 app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
 
 app.get("/",(req,res)=>{
-    return res.render("home");
+    return res.render("home",{
+        user: req.user,
+    });
 })
 
 app.use("/user" , userRoute);
