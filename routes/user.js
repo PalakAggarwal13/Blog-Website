e8bec18251464f7ff9f3e1c65236479c136743e5
Palak aggarwal <palakaggarwalaol@gmail.com>
2025-05-30 18:20:15 +0530

    token generated on signin page

--- routes/user.js
+++ routes/user.js
@@ -25,10 +25,15 @@ router.post("/signup", async(req,res)=>{
 
 router.post("/signin" , async(req,res)=>{
     const {email,password} = req.body;
-    const user = await User.matchPasswordAndGenerateToken(email,password);
-
-    console.log("User",user);
-    return res.redirect("/");
+    try{
+    const token = await User.matchPasswordAndGenerateToken(email,password);
+    return res.cookie("token",token).redirect("/");
+    }catch(error){
+        return res.render("signin",{
+            error: "Incorect Email or Password",
+        })
+    }
+  
 })
 
 
