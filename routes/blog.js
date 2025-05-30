f3cd7cba14ad00bf9d93c699b95acaa4bd7a028e
Palak aggarwal <palakaggarwalaol@gmail.com>
2025-05-30 19:37:57 +0530

    coverImage option on blog

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,21 @@
 const {Router} = require("express");
+const multer = require("multer");
+const path = require("path");
+const Blog = require("../models/blog");
 
 const router = Router();
+
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,path.resolve(`./public/uploads/${req.user._id}`));
+    },
+    filename: function(req,file,cb){
+        const fileName = `${Date.now()}-${file.originalname}`;
+        cb(null,fileName);
+    }
+})  
+
+const upload = multer({storage});
 
 router.get("/add-new",(req,res)=>{
     return res.render("addBlog" , {
@@ -8,6 +23,14 @@ router.get("/add-new",(req,res)=>{
     })
 })
 
-router.post("/",(req,res)=>{
-   return res.redirect("/");
+router.post("/",upload.single("coverImage"),async(req,res)=>{
+    const {title,body} = req.body
+    const blog = await Blog.create({
+        body,
+        title,
+        createdBy:req.user._id,
+        coverimageURL:`/uploads/${req.file.filename}`
+    })
+    
+   return res.redirect(`/blog/${blog._id}`);
 })
